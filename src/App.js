0fc3516ccdbcf39b47e3cc8e957a597035eef0da
shivam27 <shivampurohit900@gmail.com>
2022-12-18 00:54:47 +0530

    yeah changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import ChatBot from './components/ChatBot';
 import ChatPage from './components/ChatPage';
-import MacBookPro141 from './components/mac-book-pro141';
+
 function App() {
   return (
     <><div>
-      <MacBookPro141> </MacBookPro141> {/* homepage*/}
+      
     </div><div>
         <ChatBot></ChatBot>  {/*on the right and fixed */}
 
