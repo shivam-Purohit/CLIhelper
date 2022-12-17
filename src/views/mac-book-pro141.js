083c0fc34606a05292f4ba09473cb18db4c235f5
Deep Poharkar <deeppoharkar21@gmail.com>
2022-12-18 00:46:57 +0530

    updated from css modules to css

---
+++ src/views/mac-book-pro141.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './mac-book-pro141.css'
+
+const MacBookPro141 = (props) => {
+  return (
+    <div className="mac-book-pro141-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="mac-book-pro141-mac-book-pro141">
+        <div className="mac-book-pro141-navbar8">
+          <div className="mac-book-pro141-frame28">
+            <div className="mac-book-pro141-group4">
+              <span className="mac-book-pro141-text Text">
+                <span>How it works</span>
+              </span>
+            </div>
+            <span className="mac-book-pro141-text02 Text">
+              <span>About Us</span>
+            </span>
+          </div>
+          <div className="mac-book-pro141-frame175">
+            <button className="mac-book-pro141-button">
+              <span className="mac-book-pro141-text04 Button">
+                <span>Login</span>
+              </span>
+            </button>
+            <button className="mac-book-pro141-button1">
+              <span className="mac-book-pro141-text06 Button">
+                <span>Connect</span>
+              </span>
+            </button>
+          </div>
+        </div>
+        <img
+          alt="cli212283"
+          src="/playground_assets/cli212283-siqj-200h.png"
+          className="mac-book-pro141-cli21"
+        />
+        <img
+          alt="Rectangle59285"
+          src="/playground_assets/rectangle59285-0v6.svg"
+          className="mac-book-pro141-rectangle5"
+        />
+        <img
+          alt="Ellipse41429"
+          src="/playground_assets/ellipse41429-e7dt-200w.png"
+          className="mac-book-pro141-ellipse4"
+        />
+        <img
+          alt="Ellipse51429"
+          src="/playground_assets/ellipse51429-t2ii-200w.png"
+          className="mac-book-pro141-ellipse5"
+        />
+        <img
+          alt="Ellipse61429"
+          src="/playground_assets/ellipse61429-c0e-200w.png"
+          className="mac-book-pro141-ellipse6"
+        />
+        <span className="mac-book-pro141-text08">
+          <span>Confused about CLI commands?</span>
+        </span>
+        <span className="mac-book-pro141-text10">
+          <span>
+            <span>
+              Struggling to get familiar with CLI commands?
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <br></br>
+            <span>Confused what it does or where should you use it?</span>
+            <br></br>
+            <span>Don’t worry, we got you all covered!</span>
+          </span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default MacBookPro141
